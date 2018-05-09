b6ec6490f92a6f5e1d1819dfdde55adeaecdf328
AnilRdyAleti <AnilRdyAleti@github.com>
2018-05-08 22:28:04 -0400

    Added selection activity

---
+++ app/src/main/java/com/example/anilreddy/rxjavasampleapp/SelectionActivity.java
@@ -0,0 +1,32 @@
+package com.example.anilreddy.rxjavasampleapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class SelectionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_selection);
+    }
+
+    public void startOperatorsActivity(View view) {
+    }
+
+    public void startNetworkingActivity(View view) {
+    }
+
+    public void startRxBusActivity(View view) {
+    }
+
+    public void startPaginationActivity(View view) {
+    }
+
+    public void startComposeOperator(View view) {
+    }
+
+    public void startSearchActivity(View view) {
+    }
+}
