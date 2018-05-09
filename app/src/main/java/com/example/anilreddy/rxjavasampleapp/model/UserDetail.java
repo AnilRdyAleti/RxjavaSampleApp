bc86642226d9f62cd1868ece44208ad2c5f1afaa
AnilRdyAleti <AnilRdyAleti@github.com>
2018-05-09 10:02:02 -0400

    completed Car, UserDetail in model package

--- app/src/main/java/com/example/anilreddy/rxjavasampleapp/model/UserDetail.java
+++ app/src/main/java/com/example/anilreddy/rxjavasampleapp/model/UserDetail.java
@@ -1,4 +1,17 @@
 package com.example.anilreddy.rxjavasampleapp.model;
 
 public class UserDetail {
+    public long id;
+    public String firstName;
+    public String lastName;
+
+    @Override
+    public String toString() {
+        return "UserDetail{" +
+                "id=" + id +
+                ", firstName = '" + firstName + '\'' +
+                ", lastName = '" + lastName + '\'' +
+                '}';
+
+    }
 }
--- app/src/main/java/com/example/anilreddy/rxjavasampleapp/utils/Utils.java
+++ app/src/main/java/com/example/anilreddy/rxjavasampleapp/utils/Utils.java
@@ -1,5 +1,6 @@
 package com.example.anilreddy.rxjavasampleapp.utils;
 
+import com.example.anilreddy.rxjavasampleapp.model.ApiUser;
 import com.example.anilreddy.rxjavasampleapp.model.User;
 
 import java.util.ArrayList;
@@ -16,21 +17,43 @@ public class Utils {
         List<User> userList = new ArrayList<>();
 
         User userOne = new User();
-        userOne.firstName = "";
-        userOne.lastName = "";
+        userOne.firstName = "Anil";
+        userOne.lastName = "Reddy";
         userList.add(userOne);
 
         User userTwo = new User();
-        userOne.firstName = "";
-        userOne.lastName = "";
+        userOne.firstName = "Manish";
+        userOne.lastName = "Kumar";
         userList.add(userTwo);
 
         User userThree = new User();
-        userOne.firstName = "";
-        userOne.lastName = "";
+        userOne.firstName = "Sumit";
+        userOne.lastName = "Kumar";
         userList.add(userThree);
 
         return userList;
+    }
+
+    public static List<ApiUser> getApiUserList() {
+
+        List<ApiUser> apiUserList = new ArrayList<>();
+
+        ApiUser apiUserOne = new ApiUser();
+        apiUserOne.firstName = "Anil";
+        apiUserOne.lastName = "Reddy";
+        apiUserList.add(apiUserOne);
+
+        ApiUser apiUserTwo = new ApiUser();
+        apiUserTwo.firstName = "Manish";
+        apiUserTwo.lastName = "Kumar";
+        apiUserList.add(apiUserTwo);
+
+        ApiUser apiUserThree = new ApiUser();
+        apiUserThree.firstName = "Sumit";
+        apiUserThree.lastName = "Kumar";
+        apiUserList.add(apiUserThree);
+
+        return apiUserList;
     }
 
 }
